a4279c47cdecd24ffcb9d17fe4f2cf48ec9e4f21
yuriy <y.smirnov@worksolutions.ru>
2023-08-21 06:31:11 +0300

    ci: add parameter

--- backend/config/database.config.ts
+++ backend/config/database.config.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 export default () => ({
   postgres: {
     port: parseInt(process.env.DATABASE_PORT) || 5432,
@@ -5,5 +7,6 @@ export default () => ({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    synchronize: !!process.env.SYNCHRONIZE_ENTITIES,
   },
 });
