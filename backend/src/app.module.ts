7ee5a28ad43285edad65bb29addd5dc8f1724dc8
yuriy <y.smirnov@worksolutions.ru>
2023-08-21 07:16:31 +0300

    feat: connect database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import databaseConfig from '../config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      ignoreEnvFile: true,
       load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.postgres.host'),
+        port: configService.get('database.postgres.port'),
+        username: configService.get('database.postgres.user'),
+        password: configService.get('database.postgres.password'),
+        database: configService.get('database.postgres.database'),
+        entities: [],
+        synchronize: configService.get('database.postgres.synchronize'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
