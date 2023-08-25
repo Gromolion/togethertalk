d95950ad57f2a96bc0306a210630d0fe9c93e923
yuriy <y.smirnov@worksolutions.ru>
2023-08-25 05:19:59 +0300

    ci: change postgres variables names in .env

--- backend/config/database.config.ts
+++ backend/config/database.config.ts
@@ -3,11 +3,11 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   postgres: {
-    port: parseInt(process.env.DATABASE_PORT) || 5432,
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    port: parseInt(process.env.POSTGRES_PORT) || 5432,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB_NAME,
     synchronize: !!process.env.SYNCHRONIZE_ENTITIES,
   },
 }));
