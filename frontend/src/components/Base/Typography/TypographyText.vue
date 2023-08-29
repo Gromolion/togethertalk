f032750c0029e8c9b4938ce315cc95b088aa113c
yuriy <y.smirnov@worksolutions.ru>
2023-08-29 05:48:02 +0300

    feat: add base component for text

---
+++ frontend/src/components/Base/Typography/TypographyText.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { TypographyElements } from "@/components/Base/Typography/enum";
+
+const props = withDefaults(
+  defineProps<{
+    element: TypographyElements;
+    color: string;
+  }>(),
+  {
+    element: TypographyElements.SPAN,
+    color: "black",
+  }
+);
+</script>
+
+<template>
+  <component id="typography" :is="props.element">
+    <slot></slot>
+  </component>
+</template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+
+#typography {
+  font-family: "Montserrat", serif;
+  margin: 0;
+  color: v-bind(color);
+}
+</style>
