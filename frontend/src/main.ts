acc3abbf740a8bb623ecd7c4104307b65f85aafb
yuriy <y.smirnov@worksolutions.ru>
2023-08-29 05:47:48 +0300

    feat: add vue router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "@/App.vue";
+import ChatRoom from "@/components/ChatRoom/ChatRoom.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [{ path: "/", component: ChatRoom }];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
