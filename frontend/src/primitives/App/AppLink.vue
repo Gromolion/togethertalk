de5f1e3c92548be972205d0846e23a92809511e7
yuriy <y.smirnov@worksolutions.ru>
2023-08-30 22:35:27 +0300

    feat: app primitives

---
+++ frontend/src/primitives/App/AppLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  url: string;
+}>();
+</script>
+
+<template>
+  <router-link class="router-link" :to="props.url">
+    <slot />
+  </router-link>
+</template>
+
+<style scoped>
+.router-link {
+  text-decoration: none;
+}
+</style>
---
+++ frontend/src/primitives/App/AppView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background: #624977;
+}
+</style>
