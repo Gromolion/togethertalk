de97ea8d73e83faf82b557af29057cb4c0fd7f72
yuriy <y.smirnov@worksolutions.ru>
2023-08-30 05:33:34 +0300

    feat: add webrtc-adapter

--- frontend/src/components/ChatRoom/hooks.ts
+++ frontend/src/components/ChatRoom/hooks.ts
@@ -3,12 +3,14 @@ import usePeer from "@/hooks/usePeer";
 import { Ref, ref } from "vue";
 import { Video } from "@/components/ChatRoom/types";
 import { MediaConnection } from "peerjs";
+// noinspection ES6UnusedImports
+import adapter from "webrtc-adapter";
 
-export function useInitVideoChat() {
+export function useInitVideoChat(roomId: string) {
   const videos: Ref<Video[]> = ref([]);
   const socket = useSocket("chat-room", {
     extraHeaders: {
-      "Chat-Room-UUID": "d97a22df-dd04-40b0-8611-232a3493ef79",
+      "Chat-Room-UUID": roomId,
     },
   });
   navigator.mediaDevices
@@ -16,48 +18,53 @@ export function useInitVideoChat() {
       video: true,
       audio: false,
     })
-    .then((stream) => {
-      videos.value.push({
-        socketId: socket.id,
-        stream: stream,
-      });
+    .then(
+      (stream) => {
+        videos.value.push({
+          socketId: socket.id,
+          stream: stream,
+        });
 
-      const peer = usePeer(socket.id, "/peerjs");
+        const peer = usePeer(socket.id, "/peerjs");
 
-      peer.on(`open`, (id: string) => {
-        socket.emit(`join-room`, id);
-      });
+        peer.on(`open`, (id: string) => {
+          socket.emit(`join-room`, id);
+        });
 
-      peer.on(`call`, (call: MediaConnection) => {
-        call.answer(stream);
-        call.on(`stream`, (userVideoStream: MediaStream) => {
-          videos.value.push({
-            socketId: call.metadata.socketId,
-            stream: userVideoStream,
+        peer.on(`call`, (call: MediaConnection) => {
+          call.answer(stream);
+          call.on(`stream`, (userVideoStream: MediaStream) => {
+            videos.value.push({
+              socketId: call.metadata.socketId,
+              stream: userVideoStream,
+            });
           });
         });
-      });
 
-      socket.on(`user-connected`, (userId: string, socketId: string) => {
-        const call = peer.call(userId, stream, {
-          metadata: {
-            socketId: socket.id,
-          },
-        });
-        call.on(`stream`, (userVideoStream: MediaStream) => {
-          videos.value.push({
-            socketId: socketId,
-            stream: userVideoStream,
+        socket.on(`user-connected`, (userId: string, socketId: string) => {
+          const call = peer.call(userId, stream, {
+            metadata: {
+              socketId: socket.id,
+            },
+          });
+          call.on(`stream`, (userVideoStream: MediaStream) => {
+            videos.value.push({
+              socketId: socketId,
+              stream: userVideoStream,
+            });
           });
         });
-      });
 
-      socket.on("user-disconnected", (socketId: string) => {
-        videos.value = videos.value.filter((video) => {
-          return video.socketId !== socketId;
+        socket.on("user-disconnected", (socketId: string) => {
+          videos.value = videos.value.filter((video) => {
+            return video.socketId !== socketId;
+          });
         });
-      });
-    });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
   return videos;
 }
