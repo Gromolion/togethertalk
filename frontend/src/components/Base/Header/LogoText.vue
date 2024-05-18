c56133e1efec5381bc9574deb521895e564914c9
y.smirnov <y.smirnov@worksolutions.ru>
2024-05-18 14:51:49 +0300

    fix: logo

--- frontend/src/components/Base/Header/LogoText.vue
+++ frontend/src/components/Base/Header/LogoText.vue
@@ -3,14 +3,15 @@ import TypographyText from "@/primitives/Typography/TypographyText.vue";
 import { TypographyElements } from "@/primitives/Typography/enum";
 import AppLink from "@/primitives/App/AppLink";
 import AppRoutes from "@/storage/appState/appRoutes.ts";
+
+const logoSource = `${window.location.origin}/assets/logo.svg`;
 </script>
 
 <template>
   <div>
-    <AppLink :url="AppRoutes.getMainUrl()">
-      <TypographyText :element="TypographyElements.H2" color="white"
-        >TogetherTalk</TypographyText
-      >
+    <AppLink :url="AppRoutes.getMainUrl()" id="logoAndText">
+      <img :src="logoSource" alt="logo"/>
+      <TypographyText :element="TypographyElements.H2" color="white">TogetherTalk</TypographyText>
     </AppLink>
   </div>
 </template>
@@ -18,6 +19,16 @@ import AppRoutes from "@/storage/appState/appRoutes.ts";
 <style scoped>
 div {
   display: flex;
+  align-items: center;
+}
+
+img {
+  max-width: 70px;
+}
+
+#logoAndText {
+  display: flex;
+  gap: 8px;
   align-items: center;
 }
 </style>
