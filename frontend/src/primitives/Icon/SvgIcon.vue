4871541b8b9586aa06dd9492e405207a69e914b4
y.smirnov <y.smirnov@worksolutions.ru>
2024-05-18 21:47:14 +0300

    feat: add icon primitive

---
+++ frontend/src/primitives/Icon/SvgIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  type: string,
+  fill?: string,
+  stroke?: string,
+}>();
+</script>
+
+<template>
+  <img alt="" :src="`${window.location.origin}/assets/icons${props.type}`" />
+</template>
+
+<style scoped>
+ img {
+   fill: v-bind('fill');
+   stroke: v-bind('stroke');
+ }
+</style>
---
+++ frontend/src/primitives/Icon/enum.ts
@@ -0,0 +1,4 @@
+export enum IconsTypes {
+  CHECK_16 = "check_16.svg",
+  DISMISS_16 = "dismiss_16.svg",
+}
